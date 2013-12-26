e00cef2c6d162220a6e857832288b0ae731fd3b8
claudia.colella90 <claudia.colella90@c582124a-f190-42ea-9c42-5bfdfb864691>
2013-12-26 15:07:46 +0000

    TagBuilder

---
+++ Explore/src/it/polimi/dmw/cac/explore/controller/builder/TagBuilder.java
@@ -0,0 +1,26 @@
+package it.polimi.dmw.cac.explore.controller.builder;
+
+import org.slim3.datastore.Datastore;
+
+import it.polimi.dmw.cac.explore.model.Tag;
+
+public class TagBuilder {
+    
+    private Tag tag ;
+    
+    private TagBuilder(){
+        tag = new Tag();
+    }
+    
+    public static TagBuilder create() {
+        return new TagBuilder();
+    }
+    
+    public TagBuilder Name(String name){
+        tag.setName(name);
+        return this;
+    }
+    public Key store() {
+        return Datastore.put(tag);
+    }
+}
