79adaa96fcb93384c38c98872340954efe279f79
claudia.colella90 <claudia.colella90@c582124a-f190-42ea-9c42-5bfdfb864691>
2013-12-26 14:41:52 +0000

    ReviewBuilder

--- Explore/src/it/polimi/dmw/cac/explore/controller/builder/ReviewBuilder.java
+++ Explore/src/it/polimi/dmw/cac/explore/controller/builder/ReviewBuilder.java
@@ -1,7 +1,44 @@
 package it.polimi.dmw.cac.explore.controller.builder;
 
-@Model(schemaVersion = 1 )
+import java.util.Date;
+
+import it.polimi.dmw.cac.explore.model.Review;
+import it.polimi.dmw.cac.explore.model.User;
+
+import org.slim3.datastore.Datastore;
+
 public class ReviewBuilder {
     
-
+    private Review review ;
+    
+    public static ReviewBuilder create() {
+        return new ReviewBuilder() ;
+    }
+    
+    private ReviewBuilder() {
+       review = new Review();
+    }
+    
+    public ReviewBuilder author(User author) {
+        review.setAuthor(author) ;
+        return this ;
+    }
+    
+    public ReviewBuilder text(String text) {
+      review.setText(text) ; 
+      return this ; 
+    }
+    
+    public ReviewBuilder grade(int grade) {
+        review.setGrade(grade);
+        return this;
+    }
+    public ReviewBuilder date (Date date) {
+        review.setDate(date);
+        return this;
+    }
+    
+    public Key store() {
+        return Datastore.put(review);
+    }
 }
